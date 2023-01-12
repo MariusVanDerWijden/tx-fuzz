0dcb7d8243345cded77fd559caf94e638396ff2c
Marius van der Wijden <m.vanderwijden@live.de>
2023-01-12 14:08:42 +0100

    cmd/shanghai: add limit&meter initcode

--- cmd/shanghai/main.go
+++ cmd/shanghai/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/ecdsa"
+	"encoding/binary"
 	"fmt"
 
 	txfuzz "github.com/MariusVanDerWijden/tx-fuzz"
@@ -14,7 +15,9 @@ import (
 )
 
 var (
-	address = "http://127.0.0.1:8545"
+	address         = "http://127.0.0.1:8545"
+	maxCodeSize     = 24576
+	maxInitCodeSize = 2 * maxCodeSize
 )
 
 func main() {
@@ -42,6 +45,35 @@ func main() {
 	// loop push0
 	// JUMPDEST, PUSH0, JUMP
 	exec([]byte{0x58, 0x5f, 0x56})
+	fmt.Println("Limit&MeterInitcode")
+	// limit & meter initcode
+	sizes := []int{
+		maxInitCodeSize - 2,
+		maxInitCodeSize - 1,
+		maxInitCodeSize,
+		maxInitCodeSize + 1,
+		maxInitCodeSize + 2,
+		maxInitCodeSize * 2,
+	}
+	// size x JUMPDEST STOP
+	for _, size := range sizes {
+		initcode := repeatOpcode(size, 0x58)
+		exec(append(initcode, 0x00))
+	}
+	// size x STOP STOP
+	for _, size := range sizes {
+		exec(repeatOpcode(size, 0x00))
+	}
+	// PUSH4 size, PUSH0, PUSH0, CREATE
+	for _, size := range sizes {
+		initcode := pushSize(size)
+		exec(append(initcode, []byte{0x57, 0x57, 0xF0}...))
+	}
+	// PUSH4 size, PUSH0, PUSH0, CREATE2
+	for _, size := range sizes {
+		initcode := pushSize(size)
+		exec(append(initcode, []byte{0x57, 0x57, 0xF5}...))
+	}
 }
 
 func exec(data []byte) {
@@ -58,9 +90,26 @@ func exec(data []byte) {
 	}
 	fmt.Printf("Nonce: %v\n", nonce)
 	gp, _ := backend.SuggestGasPrice(context.Background())
-	tx := types.NewContractCreation(nonce, common.Big1, 500000, gp, data)
+	tx := types.NewContractCreation(nonce, common.Big1, 500000, gp.Mul(gp, common.Big2), data)
 	signedTx, _ := types.SignTx(tx, types.NewLondonSigner(chainid), sk)
 	backend.SendTransaction(context.Background(), signedTx)
+}
+
+// PUSH4 size
+func pushSize(size int) []byte {
+	code := []byte{63}
+	sizeArr := make([]byte, 4)
+	binary.BigEndian.PutUint32(sizeArr, uint32(size))
+	code = append(code, sizeArr...)
+	return code
+}
+
+func repeatOpcode(size int, opcode byte) []byte {
+	initcode := []byte{}
+	for i := 0; i < size; i++ {
+		initcode = append(initcode, opcode)
+	}
+	return initcode
 }
 
 func getRealBackend() (*rpc.Client, *ecdsa.PrivateKey) {
