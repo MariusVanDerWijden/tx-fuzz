dcc997d5fe92ab3699e8feaed77fb080f1676c1f
Marius van der Wijden <m.vanderwijden@live.de>
2023-10-13 15:57:29 +0200

    spammer: removed unused function

--- spammer/helper.go
+++ spammer/helper.go
@@ -9,7 +9,6 @@ import (
 	txfuzz "github.com/MariusVanDerWijden/tx-fuzz"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -26,29 +25,6 @@ func SendTx(sk *ecdsa.PrivateKey, backend *ethclient.Client, to common.Address, 
 	}
 	gp, _ := backend.SuggestGasPrice(context.Background())
 	tx := types.NewTransaction(nonce, to, value, 500000, gp, nil)
-	signedTx, _ := types.SignTx(tx, types.NewEIP155Signer(chainid), sk)
-	return backend.SendTransaction(context.Background(), signedTx)
-}
-
-func unstuck(sk *ecdsa.PrivateKey, backend *ethclient.Client, to common.Address, value, gasPrice *big.Int) error {
-	sender := crypto.PubkeyToAddress(sk.PublicKey)
-	blocknumber, err := backend.BlockNumber(context.Background())
-	if err != nil {
-		return err
-	}
-	nonce, err := backend.NonceAt(context.Background(), sender, big.NewInt(int64(blocknumber)))
-	if err != nil {
-		return err
-	}
-	chainid, err := backend.ChainID(context.Background())
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Acc: %v Nonce: %v\n", sender, nonce)
-	if gasPrice == nil {
-		gasPrice, _ = backend.SuggestGasPrice(context.Background())
-	}
-	tx := types.NewTransaction(nonce, to, value, 21000, gasPrice, nil)
 	signedTx, _ := types.SignTx(tx, types.NewEIP155Signer(chainid), sk)
 	return backend.SendTransaction(context.Background(), signedTx)
 }
