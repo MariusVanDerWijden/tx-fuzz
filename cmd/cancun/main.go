90859f534af57ab5ae19b8dc81b8ad64a5380641
Marius van der Wijden <m.vanderwijden@live.de>
2024-05-10 13:26:41 +0200

    cmd/prague: started work on prague tests

--- cmd/cancun/main.go
+++ cmd/cancun/main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"context"
-	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
-	"math/big"
 	"time"
 
-	txfuzz "github.com/MariusVanDerWijden/tx-fuzz"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/kzg4844"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
-)
-
-const (
-	maxDataPerBlob = 1 << 17 // 128Kb
 )
 
 var (
@@ -185,7 +171,6 @@ func test4788() {
 }
 
 func test4844() {
-	// deployProxy
 	addr, err := deployBlobProxy()
 	if err != nil {
 		panic(err)
@@ -247,12 +232,6 @@ func test4844_precompile() {
 	}
 }
 
-func wait(tx *types.Transaction) {
-	client, _ := getRealBackend()
-	backend := ethclient.NewClient(client)
-	bind.WaitMined(context.Background(), backend, tx)
-}
-
 func precompileParamsToBytes(commitment kzg4844.Commitment, claim kzg4844.Claim, proof kzg4844.Proof, point kzg4844.Point) []byte {
 	bytes := make([]byte, 192)
 	versionedHash := kZGToVersionedHash(commitment)
@@ -308,90 +287,6 @@ func kZGToVersionedHash(kzg kzg4844.Commitment) common.Hash {
 	h[0] = 0x01
 
 	return h
-}
-
-func exec(addr common.Address, data []byte, blobs bool) *types.Transaction {
-	cl, sk := getRealBackend()
-	backend := ethclient.NewClient(cl)
-	sender := common.HexToAddress(txfuzz.ADDR)
-	nonce, err := backend.PendingNonceAt(context.Background(), sender)
-	if err != nil {
-		panic(err)
-	}
-	chainid, err := backend.ChainID(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Nonce: %v\n", nonce)
-	gp, err := backend.SuggestGasPrice(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	tip, err := backend.SuggestGasTipCap(context.Background())
-	if err != nil {
-		tip = big.NewInt(100000000)
-		//panic(err)
-	}
-	var rlpData []byte
-	var _tx *types.Transaction
-	if blobs {
-		blob, err := randomBlobData()
-		if err != nil {
-			panic(err)
-		}
-		//nonce = nonce - 2
-		tx := txfuzz.New4844Tx(nonce, &addr, 500000, chainid, tip.Mul(tip, common.Big1), gp.Mul(gp, common.Big1), common.Big0, data, big.NewInt(1_000_000), blob, make(types.AccessList, 0))
-		signedTx, _ := types.SignTx(tx, types.NewCancunSigner(chainid), sk)
-		rlpData, err = signedTx.MarshalBinary()
-		if err != nil {
-			panic(err)
-		}
-		_tx = signedTx
-	} else {
-		tx := types.NewTx(&types.DynamicFeeTx{ChainID: chainid, Nonce: nonce, GasTipCap: tip, GasFeeCap: gp, Gas: 500000, To: &addr, Data: data})
-		signedTx, _ := types.SignTx(tx, types.NewCancunSigner(chainid), sk)
-		rlpData, err = signedTx.MarshalBinary()
-		if err != nil {
-			panic(err)
-		}
-		_tx = signedTx
-	}
-
-	if err := cl.CallContext(context.Background(), nil, "eth_sendRawTransaction", hexutil.Encode(rlpData)); err != nil {
-		panic(err)
-	}
-	return _tx
-}
-
-func getRealBackend() (*rpc.Client, *ecdsa.PrivateKey) {
-	// eth.sendTransaction({from:personal.listAccounts[0], to:"0xb02A2EdA1b317FBd16760128836B0Ac59B560e9D", value: "100000000000000"})
-
-	sk := crypto.ToECDSAUnsafe(common.FromHex(txfuzz.SK))
-	if crypto.PubkeyToAddress(sk.PublicKey).Hex() != txfuzz.ADDR {
-		panic(fmt.Sprintf("wrong address want %s got %s", crypto.PubkeyToAddress(sk.PublicKey).Hex(), txfuzz.ADDR))
-	}
-	cl, err := rpc.Dial(address)
-	if err != nil {
-		panic(err)
-	}
-	return cl, sk
-}
-
-func randomBlobData() ([]byte, error) {
-	val, err := rand.Int(rand.Reader, big.NewInt(maxDataPerBlob))
-	if err != nil {
-		return nil, err
-	}
-	size := int(val.Int64() * 3)
-	data := make([]byte, size)
-	n, err := rand.Read(data)
-	if err != nil {
-		return nil, err
-	}
-	if n != size {
-		return nil, fmt.Errorf("could not create random blob data with size %d: %v", size, err)
-	}
-	return data, nil
 }
 
 /*
@@ -454,47 +349,6 @@ contract Call4788 {
 func deploy4788Proxy() (common.Address, error) {
 	bytecode := "608060405234801561001057600080fd5b5061048a806100206000396000f3fe608060405234801561001057600080fd5b5060003660606000720f3df6d732807ef1319fb7b8bb8522d0beac0290506000808273ffffffffffffffffffffffffffffffffffffffff168686604051610058929190610119565b6000604051808303816000865af19150503d8060008114610095576040519150601f19603f3d011682016040523d82523d6000602084013e61009a565b606091505b509150915080600090816100ae9190610382565b5081600160006101000a81548160ff021916908315150217905550809350505050915050805190602001f35b600081905092915050565b82818337600083830152505050565b600061010083856100da565b935061010d8385846100e5565b82840190509392505050565b60006101268284866100f4565b91508190509392505050565b600081519050919050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052604160045260246000fd5b7f4e487b7100000000000000000000000000000000000000000000000000000000600052602260045260246000fd5b600060028204905060018216806101b357607f821691505b6020821081036101c6576101c561016c565b5b50919050565b60008190508160005260206000209050919050565b60006020601f8301049050919050565b600082821b905092915050565b60006008830261022e7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff826101f1565b61023886836101f1565b95508019841693508086168417925050509392505050565b6000819050919050565b6000819050919050565b600061027f61027a61027584610250565b61025a565b610250565b9050919050565b6000819050919050565b61029983610264565b6102ad6102a582610286565b8484546101fe565b825550505050565b600090565b6102c26102b5565b6102cd818484610290565b505050565b5b818110156102f1576102e66000826102ba565b6001810190506102d3565b5050565b601f82111561033657610307816101cc565b610310846101e1565b8101602085101561031f578190505b61033361032b856101e1565b8301826102d2565b50505b505050565b600082821c905092915050565b60006103596000198460080261033b565b1980831691505092915050565b60006103728383610348565b9150826002028217905092915050565b61038b82610132565b67ffffffffffffffff8111156103a4576103a361013d565b5b6103ae825461019b565b6103b98282856102f5565b600060209050601f8311600181146103ec57600084156103da578287015190505b6103e48582610366565b86555061044c565b601f1984166103fa866101cc565b60005b82811015610422578489015182556001820191506020850194506020810190506103fd565b8683101561043f578489015161043b601f891682610348565b8355505b6001600288020188555050505b50505050505056fea2646970667358221220d3505d93c72ff246e512c416145e275fe92925a05a9953337b1add26b509ec7764736f6c63430008120033"
 	return deploy(bytecode)
-}
-
-func deploy(bytecode string) (common.Address, error) {
-	cl, sk := getRealBackend()
-	backend := ethclient.NewClient(cl)
-	sender := common.HexToAddress(txfuzz.ADDR)
-	nonce, err := backend.PendingNonceAt(context.Background(), sender)
-	if err != nil {
-		return common.Address{}, err
-	}
-	chainid, err := backend.ChainID(context.Background())
-	if err != nil {
-		return common.Address{}, err
-	}
-	fmt.Printf("Nonce: %v\n", nonce)
-	gp, _ := backend.SuggestGasPrice(context.Background())
-	tx := types.NewContractCreation(nonce, common.Big0, 500000, gp.Mul(gp, common.Big2), common.Hex2Bytes(bytecode))
-	signedTx, _ := types.SignTx(tx, types.NewLondonSigner(chainid), sk)
-	if err := backend.SendTransaction(context.Background(), signedTx); err != nil {
-		return common.Address{}, err
-	}
-	return bind.WaitDeployed(context.Background(), backend, signedTx)
-}
-
-func execute(data []byte, gaslimit uint64) {
-	cl, sk := getRealBackend()
-	backend := ethclient.NewClient(cl)
-	sender := common.HexToAddress(txfuzz.ADDR)
-	nonce, err := backend.PendingNonceAt(context.Background(), sender)
-	if err != nil {
-		panic(err)
-	}
-	chainid, err := backend.ChainID(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Nonce: %v\n", nonce)
-	gp, _ := backend.SuggestGasPrice(context.Background())
-	tx := types.NewContractCreation(nonce, common.Big1, gaslimit, gp.Mul(gp, common.Big2), data)
-	signedTx, _ := types.SignTx(tx, types.NewLondonSigner(chainid), sk)
-	backend.SendTransaction(context.Background(), signedTx)
 }
 
 func Uint64ToHash(u uint64) common.Hash {
